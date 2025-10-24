45920ec807d06f188ef994bce2815b7280112dd9
Amanrawal1305 <141016643+Amanrawal1305@users.noreply.github.com>
2025-10-24 16:44:50 +0530

    Update MyProfile.jsx

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -7,11 +7,8 @@ const MyProfile = () => {
 
     const [userData, setUserData] = useState({
         name: "Aman Rawal",
-<<<<<<< HEAD
+
         image: assets/aman.png,
-=======
-        image: assets.amanpic,
->>>>>>> 63e0b91e4642bbbee75d66c1d40eb1baddba8d71
         email: 'amanrawal1305@gmail.com',
         phone: '8851736261',
         address: {
